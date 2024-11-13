c5878965cefdf94b086eb9e1a77b9a442606f956
bbkbyti <bbkbyti@gmail.com>
2024-11-13 11:09:50 -0600

    working on Player name and editing player name functionality of the game

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,27 @@
+import { useState } from 'react';
 
 import Player from './components/Player';
 import './App.css';
 
 function App() {
+
+  const [player, setPlayer] = useState({
+    'X': 'Player 1',
+    'O': 'Player 2',
+  })
+
+  const handlePlayerNameChange = (symbol, newName) => {
+    setPlayer(prevPlayer => {
+      return {
+        ...prevPlayer,
+        [symbol]: newName
+      }
+    })
+  }
   return (
     <div className="App">
-      <Player />
+      <Player initialName='Player 1' symbol='X' onChangeName={handlePlayerNameChange} />
+      <Player initialName='Player 2' symbol='O' onChangeName={handlePlayerNameChange} />
     </div>
   );
 }
--- my-app/src/components/Player.jsx
+++ my-app/src/components/Player.jsx
@@ -1,7 +1,26 @@
-import React from 'react'
+import { useState } from 'react';
 
-export default function Player() {
+export default function Player({ initialName, symbol, onChangeName }) {
+    const [playerName, setPlayerName] = useState(initialName);
+    const [isEditing, setIsEditing] = useState(false);
+
+    const handleEditClick = () => {
+        setIsEditing((editing) => !editing);
+
+        if (isEditing) {
+            onChangeName(symbol, playerName)
+        }
+    }
+
+    const handleChange = (e) => {
+        setPlayerName(e.target.value);
+    }
+
+    if (isEditing) {
+        editPlayerName = <input type='text' required value={playerName} onChange={handleChange} />
+    }
+    let editPlayerName = <span>{playerName}</span>
     return (
-        <div>Player</div>
+        <div></div>
     )
 }
