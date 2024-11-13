a07687d2add68dba5bc5715896744dcf5bb615fe
bbkbyti <bbkbyti@gmail.com>
2024-11-13 11:26:13 -0600

    working on player component and game board component added as well

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,7 +1,15 @@
 import { useState } from 'react';
+import './App.css';
 
 import Player from './components/Player';
-import './App.css';
+import GameBoard from './components/GameBoard';
+
+const derivedActivePlayer = (gameTurns) => {
+  let currentPlayer = 'X';
+
+  return currentPlayer;
+}
+
 
 function App() {
 
@@ -9,6 +17,21 @@ function App() {
     'X': 'Player 1',
     'O': 'Player 2',
   })
+
+  const [gameTurns, setGameTurns] = useState([]);
+
+  const activePlayer = derivedActivePlayer(gameTurns);
+
+  const handleSelectSquare = (rowIndex, colIndex) => {
+    setGameTurns(prevTurns => {
+      const currentPlayer = derivedActivePlayer(prevTurns);
+      const updatedTurns = [
+        { square: { row: rowIndex, col: ColIndex }, player: currentPlayer },
+        ...prevTurns
+      ]
+      return updatedTurns;
+    })
+  }
 
   const handlePlayerNameChange = (symbol, newName) => {
     setPlayer(prevPlayer => {
@@ -20,8 +43,15 @@ function App() {
   }
   return (
     <div className="App">
-      <Player initialName='Player 1' symbol='X' onChangeName={handlePlayerNameChange} />
-      <Player initialName='Player 2' symbol='O' onChangeName={handlePlayerNameChange} />
+      <Player initialName='Player 1'
+        symbol='X'
+        onChangeName={handlePlayerNameChange}
+        isActive={activePlayer} />
+      <Player initialName='Player 2'
+        symbol='O'
+        onChangeName={handlePlayerNameChange}
+        isActive={activePlayer} />
+      <GameBoard />
     </div>
   );
 }
